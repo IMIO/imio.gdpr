4dcbd7e04483145701b8023ad4968cf0003abf7c
Benoit Suttor <bsuttor@imio.be>
2020-01-17 13:55:44 +0100

    Flake8

--- src/imio/gdpr/testing.py
+++ src/imio/gdpr/testing.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from imio.gdpr.interfaces import IImioGdprLayer
-from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE as PLONE_FIXTURE  # noqa
+from plone.app.contenttypes.testing import (
+    PLONE_APP_CONTENTTYPES_FIXTURE as PLONE_FIXTURE,
+)
 from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
 from plone.app.testing import applyProfile
 from plone.app.testing import FunctionalTesting
@@ -21,39 +23,34 @@ class ImioGdprLayer(PloneWithPackageLayer):
         # The z3c.autoinclude feature is disabled in the Plone fixture base
         # layer.
         import plone.app.dexterity
+
         self.loadZCML(package=plone.app.dexterity)
         self.loadZCML(package=imio.gdpr)
 
     def setUpPloneSite(self, portal):
         directlyProvides(portal.REQUEST, IImioGdprLayer)  # noqa
-        applyProfile(portal, 'imio.gdpr:default')
+        applyProfile(portal, "imio.gdpr:default")
 
 
 IMIO_GDPR_FIXTURE = ImioGdprLayer(
-    name='IMIO_GDPR_FIXTURE',
-    zcml_filename='testing.zcml',
+    name="IMIO_GDPR_FIXTURE",
+    zcml_filename="testing.zcml",
     zcml_package=imio.gdpr,
-    gs_profile_id='imio.gdpr:testing'
+    gs_profile_id="imio.gdpr:testing",
 )
 
 
 IMIO_GDPR_INTEGRATION_TESTING = IntegrationTesting(
-    bases=(IMIO_GDPR_FIXTURE,),
-    name='ImioGdprLayer:IntegrationTesting',
+    bases=(IMIO_GDPR_FIXTURE,), name="ImioGdprLayer:IntegrationTesting"
 )
 
 
 IMIO_GDPR_FUNCTIONAL_TESTING = FunctionalTesting(
-    bases=(IMIO_GDPR_FIXTURE,),
-    name='ImioGdprLayer:FunctionalTesting',
+    bases=(IMIO_GDPR_FIXTURE,), name="ImioGdprLayer:FunctionalTesting"
 )
 
 
 IMIO_GDPR_ACCEPTANCE_TESTING = FunctionalTesting(
-    bases=(
-        IMIO_GDPR_FIXTURE,
-        REMOTE_LIBRARY_BUNDLE_FIXTURE,
-        z2.ZSERVER_FIXTURE,
-    ),
-    name='ImioGdprLayer:AcceptanceTesting',
+    bases=(IMIO_GDPR_FIXTURE, REMOTE_LIBRARY_BUNDLE_FIXTURE, z2.ZSERVER_FIXTURE),
+    name="ImioGdprLayer:AcceptanceTesting",
 )
