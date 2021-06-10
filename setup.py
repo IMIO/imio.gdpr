b55318733b019623dad1edc46491235d4d24ea0b
Christophe Boulanger <christophe.boulanger@imio.be>
2021-06-10 14:50:17 +0200

    Back to development: 1.1.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='imio.gdpr',
-    version='1.1.1',
+    version='1.1.2.dev0',
     description="View and configuration panel to set GDPR information",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
